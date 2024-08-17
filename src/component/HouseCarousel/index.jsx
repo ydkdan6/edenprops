ffd7ca06246c1148945a7e85d127c1b6ad0cae31
ydkdan6 <ydkdan6@gmail.com>
2024-08-17 14:35:18 +0100

    added search bar for pc view

--- src/component/HouseCarousel/index.jsx
+++ src/component/HouseCarousel/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Text from '../../utils/TextInput';
 import HouseCard from '../HouseCard';
+import Search from '../../component/Search';
 
 const HouseCarousel = () => {
   const houses = [
@@ -44,7 +45,11 @@ const HouseCarousel = () => {
 
   return (
     <div className="container my-[100px] mx-auto py-4 h-[350px] ">
+        <div className='flex justify-between items-center'>
         <Text className='m-5 text-xl'>Near You</Text>
+        <Search />
+        </div>
+        
       <div className="overflow-x-scroll flex flex-nowrap w-screen h-[345px] scrollbar-hide">
         <div className="flex flex-nowrap h-full mt-0">
           {houses.map((house, index) => (
---
+++ src/component/Search/index.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+import './style.css'; // Import the CSS file for styling
+
+const Search = () => {
+  const [query, setQuery] = useState('');
+
+  const handleInputChange = (event) => {
+    setQuery(event.target.value);
+  };
+
+  const handleSearch = () => {
+    // Handle search functionality here
+    console.log('Search query:', query);
+  };
+
+  return (
+    <div className="search hidden md:block">
+      <input
+        type="text"
+        className="search__input"
+        placeholder="Search"
+        value={query}
+        onChange={handleInputChange}
+      />
+      <button
+        className="search__button"
+        onClick={handleSearch}
+      >
+        <svg
+          className="search__icon"
+          aria-hidden="true"
+          viewBox="0 0 24 24"
+        >
+          <g>
+            <path d="M21.53 20.47l-3.66-3.66C19.195 15.24 20 13.214 20 11c0-4.97-4.03-9-9-9s-9 4.03-9 9 4.03 9 9 9c2.215 0 4.24-.804 5.808-2.13l3.66 3.66c.147.146.34.22.53.22s.385-.073.53-.22c.295-.293.295-.767.002-1.06zM3.5 11c0-4.135 3.365-7.5 7.5-7.5s7.5 3.365 7.5 7.5-3.365 7.5-7.5 7.5-7.5-3.365-7.5-7.5z"></path>
+          </g>
+        </svg>
+      </button>
+    </div>
+  );
+};
+
+export default Search;
