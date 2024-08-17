458a5562a839d112099da2eaa9b2fb1dd5883a51
ydkdan6 <ydkdan6@gmail.com>
2024-08-17 10:13:34 +0100

    Made changes

--- src/NotFound.jsx
+++ src/NotFound.jsx
@@ -4,17 +4,16 @@ import Image from './utils/Image';
 
 const NotFoundPage = () => {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen text-center bg-gray-100">
+    <div className="w-screen h-[90%] text-center bg-gray-100 flex flex-col justify-center items-center">
       <Image
-        src="/path/to/404-image.png" // Replace with the path to your 404 image
-        alt="404 Not Found"
-        className="w-1/2 max-w-md"
+        src="/404.png"
+        className="w-[100%] h-full  md:w-[80%] md:h-auto md:m-auto"
       />
-      <h1 className="text-4xl font-bold mt-6">404 - Page Not Found</h1>
       <p className="mt-4">The page you are looking for does not exist.</p>
-      <Link to="/" className="mt-6 text-blue-500 underline">Go back to Home</Link>
+      <Link to="/" className="mt-6 text-orange-500 underline">Go back to Home</Link>
     </div>
   );
 };
 
 export default NotFoundPage;
+
