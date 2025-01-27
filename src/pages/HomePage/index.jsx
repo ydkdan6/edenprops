03bcde5e5be6667397baa4de72aa8e9ad64eae44
ydkdan6 <ydkdan6@gmail.com>
2025-01-27 15:10:16 +0100

    trying to fix mobile view

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -18,7 +18,7 @@ function HomePage() {
       
       {/* Other components for routes go here */}
       <HouseCarousel  />
-      <Text className='m-1 text-4xl text-center underline'>More listing for you</Text>
+      <Text className='m-1 text-4xl text-center text-xl underline'>More listing for you</Text>
       <PropertyGrid />
       <Sponsors />
       <SearchBtn />
